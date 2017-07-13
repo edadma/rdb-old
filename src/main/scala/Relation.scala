4922628c6b128f05e0ac124e6b01c37dd3d2af9c
Edward A Maxedon <edadma@gmail.com>
2017-07-13 19:19:46 -0400

    a relation is a set of tuples

--- src/main/scala/Relation.scala
+++ src/main/scala/Relation.scala
@@ -1,7 +1,7 @@
 package xyz.hyperreal.rdb
 
 
-trait Relation extends Iterable[Tuple] {
+trait Relation extends collection.Set[Tuple] {
 
 	def metadata: Metadata
 
@@ -9,4 +9,11 @@ trait Relation extends Iterable[Tuple] {
 
 }
 
-abstract class AbstractRelation extends Relation
+abstract class AbstractRelation extends Relation {
+
+	def -( elem: Tuple ) = sys.error( "unsupported" )
+
+	def +( elem: Tuple ) = sys.error( "unsupported" )
+
+	def contains( elem: Tuple ) = iterator contains elem	// extending classes can override with a more effecient implementation
+}
