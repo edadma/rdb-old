d95101b57e3c670f900d7ff4b9ac87476de80c67
Edward A Maxedon <edadma@gmail.com>
2017-07-15 05:23:01 -0400

    fix

--- src/main/scala/SortedTupleseq.scala
+++ src/main/scala/SortedTupleseq.scala
@@ -9,19 +9,20 @@ class SortedTupleseq( relation: Relation, fields: List[Int], ascending: Boolean 
 
 	val types = relation.metadata.header map (_.typ)
 
-	private def compare( left: Tuple, right: Tuple, fields: List[Int] ): Boolean = {
-		fields match {
-			case Nil => false
-			case f :: tail =>
-				types(f).compare( left(f), right(f) ) match {
-					case -1 => ascending
-					case 0 => compare( left, right, tail )
-					case 1 => !ascending
-				}
-		}
-	}
+	def lt( left: Tuple, right: Tuple ) = {
+		def compare( fields: List[Int] ): Boolean =
+			fields match {
+				case Nil => false
+				case f :: tail =>
+					types(f).compare( left(f), right(f) ) match {
+						case -1 => ascending
+						case 0 => compare( tail )
+						case 1 => !ascending
+					}
+			}
 
-	def lt( left: Tuple, right: Tuple ) = compare( left, right, fields )
+		compare( fields )
+	}
 
 	def iterator = {
 		val rows = new ArrayBuffer[Tuple]
