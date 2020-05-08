a956cfbba587539192c2ba331c9773c85720846f
Edward A Maxedon, Sr <edadma@vinctus.com>
2020-05-08 07:18:50 -0400

    unmerged changes from old dev branch

--- src/main/scala/Relation.scala
+++ src/main/scala/Relation.scala
@@ -1,33 +1,17 @@
 package xyz.hyperreal.rdb_sjs
 
-
 trait Relation extends collection.Set[Tuple] {
 
-	def metadata: Metadata
+  def metadata: Metadata
 
-	def collect: Relation
+  def collect: Relation
 
-	def iterator( context: List[Tuple] ): Iterator[Tuple]
+  def iterator(context: List[Tuple]): Iterator[Tuple]
 
-	def iteratorEq( context: List[Tuple], idx: Int, v: AnyRef ): Iterator[Tuple]
-
-	def iteratorNe( context: List[Tuple], idx: Int, v: AnyRef ): Iterator[Tuple]
-
-	def iteratorLt( context: List[Tuple], idx: Int, v: AnyRef ): Iterator[Tuple]
-
-	def iteratorLte( context: List[Tuple], idx: Int, v: AnyRef ): Iterator[Tuple]
-
-	def iteratorGlt( context: List[Tuple], idx: Int, v: AnyRef ): Iterator[Tuple]
-
-	def iteratorGlte( context: List[Tuple], idx: Int, v: AnyRef ): Iterator[Tuple]
-
-	def iteratorGt( context: List[Tuple], idx: Int, v: AnyRef ): Iterator[Tuple]
-
-	def iteratorGte( context: List[Tuple], idx: Int, v: AnyRef ): Iterator[Tuple]
-
-	def iteratorLgt( context: List[Tuple], idx: Int, v: AnyRef ): Iterator[Tuple]
-
-	def iteratorLgte( context: List[Tuple], idx: Int, v: AnyRef ): Iterator[Tuple]
+  def boundedIterator(context: List[Tuple],
+                      bounds: Seq[Symbol],
+                      idx: Int,
+                      v: AnyRef): Iterator[Tuple]
 }
 
 abstract class AbstractRelation extends Relation {
@@ -36,35 +20,24 @@ abstract class AbstractRelation extends Relation {
 //
 //	def +( elem: Tuple ) = sys.error( "unsupported" )
 
-	def contains( elem: Tuple ) = iterator contains elem	// extending classes can override with a more effecient implementation
+  def contains(elem: Tuple) =
+    iterator contains elem // extending classes can override with a more effecient implementation
 
-	def diff( that: collection.Set[Tuple] ) = filterNot (that contains)	// extending classes can override with a more effecient implementation
+  def diff(that: collection.Set[Tuple]) =
+    filterNot(that contains) // extending classes can override with a more effecient implementation
 
-	def collect = new ConcreteRelation( metadata.header, iterator.toList )
+  def collect = new ConcreteRelation(metadata.header, iterator.toList)
 
-	def iterator = iterator( Nil )
+  def iterator = iterator(Nil)
 
-	def iteratorEq( context: List[Tuple], idx: Int, v: AnyRef ) = iterator( context ) filter (_(idx) == v)
+  def boundedIterator(context: List[Tuple],
+                      bounds: Seq[Symbol],
+                      idx: Int,
+                      v: AnyRef) = ns
 
-	def iteratorNe( context: List[Tuple], idx: Int, v: AnyRef ) = iterator( context ) filter (_(idx) != v)
+  def ns = sys.error("not supported yet")
 
-	def iteratorLt( context: List[Tuple], idx: Int, v: AnyRef ) = ns
-
-	def iteratorLte( context: List[Tuple], idx: Int, v: AnyRef ) = ns
-
-	def iteratorGlt( context: List[Tuple], idx: Int, v: AnyRef ) = na( "G<" )
-
-	def iteratorGlte( context: List[Tuple], idx: Int, v: AnyRef ) = na( "G<=" )
-
-	def iteratorGt( context: List[Tuple], idx: Int, v: AnyRef ) = ns
-
-	def iteratorGte( context: List[Tuple], idx: Int, v: AnyRef ) = ns
-
-	def iteratorLgt( context: List[Tuple], idx: Int, v: AnyRef ) = na( "L>" )
-
-	def iteratorLgte( context: List[Tuple], idx: Int, v: AnyRef ) = na( "L>=" )
-
-	def ns = sys.error( "not supported yet, working on it" )
-
-	def na( comp: String ) = sys.error( s"comparison '$comp' can only be use with a base relation column that is indexed" )
+  def na(comp: String) =
+    sys.error(
+      s"comparison '$comp' can only be use with a base relation column that is indexed")
 }
