8c2ea2572c424f0cb92828d21703a0d4a5685e2e
Edward A Maxedon, Sr <edadma@vinctus.com>
2020-05-11 13:36:42 -0400

    ER

--- src/main/scala/ERDAST.scala
+++ src/main/scala/ERDAST.scala
@@ -1,12 +1,32 @@
 package xyz.hyperreal.rdb_sjs
 
+import scala.util.parsing.input.Positional
+
 abstract class ERDAST
 case class DefinitionERD(blocks: List[BlockERD]) extends ERDAST
 
 abstract class BlockERD extends ERDAST
 case class TypeBlockERD(name: Ident,
                         underlying: Ident,
-                        condition: ExpressionRED)
+                        condition: ExpressionERD)
     extends BlockERD
 
-abstract class ExpressionRED extends ERDAST
+abstract class ExpressionERD extends ERDAST with Positional
+
+case class VariableExpressionERD(name: Ident) extends ExpressionERD
+case class FloatLiteralERD(n: String) extends ExpressionERD
+case class IntegerLiteralERD(n: String) extends ExpressionERD
+case class StringLiteralERD(s: String) extends ExpressionERD
+case class NotExpressionERD(expr: ExpressionERD) extends ExpressionERD
+case class ComparisonExpressionERD(first: ExpressionERD,
+                                   comps: List[(String, ExpressionERD)])
+    extends ExpressionERD
+case class AndExpressionERD(left: ExpressionERD, right: ExpressionERD)
+    extends ExpressionERD
+case class OrExpressionERD(left: ExpressionERD, right: ExpressionERD)
+    extends ExpressionERD
+
+abstract class TypeSpecifierERD extends ERDAST with Positional
+case class SimpleTypeERD(name: Ident) extends TypeSpecifierERD
+
+case class EntityFieldERD(name: Ident, typ: TypeSpecifierERD) extends ERDAST
--- src/main/scala/ERDParser.scala
+++ src/main/scala/ERDParser.scala
@@ -19,19 +19,22 @@ class ERDParser extends RegexParsers {
     _.pos
   }
 
-  def number: Parser[ValueExpression] =
-    positioned("""\-?\d+(\.\d*)?""".r ^^ {
-      case n if n contains '.' => FloatLit(n)
-      case n                   => IntegerLit(n)
+  def number: Parser[ExpressionERD] =
+    positioned("""(?:\d+(?:\.\d+)?|\.\d+)(?:(?:e|E)(?:\+|-)?\d+)?""".r ^^ {
+      case n if (n contains '.') || (n contains 'e') || (n contains 'E') =>
+        FloatLiteralERD(n)
+      case n => IntegerLiteralERD(n)
     })
 
-  def string: Parser[ValueExpression] =
+  def string =
     positioned(
       (("'" ~> """[^'\n]*""".r <~ "'") |
-        ("\"" ~> """[^"\n]*""".r <~ "\"")) ^^ StringLit)
+        ("\"" ~> """[^"\n]*""".r <~ "\"")) ^^ StringLiteralERD)
 
   def ident =
     positioned("""[a-zA-Z_#$][a-zA-Z0-9_#$]*""".r ^^ Ident)
+
+  def variable = ident ^^ VariableExpressionERD
 
   def definition = rep1(block) ^^ DefinitionERD
 
@@ -47,25 +50,33 @@ class ERDParser extends RegexParsers {
   def boolCondition =
     orCondition ~ rep("and" ~> orCondition) ^^ {
       case first ~ rest =>
+        rest.foldLeft(first) { case (l, r) => AndExpressionERD(l, r) }
     }
 
   def orCondition =
-    compCondition ~ rep("or" ~ compCondition)
+    compCondition ~ rep("or" ~> compCondition) ^^ {
+      case first ~ rest =>
+        rest.foldLeft(first) { case (l, r) => OrExpressionERD(l, r) }
+    }
 
   def compCondition =
-    notCondition ~ rep(("<" | "<=") ~ notCondition)
+    positioned(notCondition ~ rep(("<" | "<=") ~ notCondition) ^^ {
+      case first ~ Nil => first
+      case first ~ rest =>
+        ComparisonExpressionERD(first, rest map { case c ~ r => (c, r) })
+    })
 
   def notCondition =
-    "not" ~ primaryCondition |
+    positioned("not" ~> primaryCondition ^^ NotExpressionERD) |
       primaryCondition
 
-  def primaryCondition = ident | number
+  def primaryCondition: Parser[ExpressionERD] = variable | number
 
   def entityBlock = "entity" ~ ident ~ "{" ~ rep1(field) ~ "}"
 
   def field = ident ~ ":" ~ typeSpec
 
-  def typeSpec = ident
+  def typeSpec = ident ^^ SimpleTypeERD
 
   def parseFromString[T](src: String, grammar: Parser[T]) =
     parseAll(grammar, new CharSequenceReader(src)) match {
